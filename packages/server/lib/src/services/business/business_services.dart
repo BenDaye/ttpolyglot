3953707efbc5a01a9dd03ac88463227390f89250
chuaer <1063195568@qq.com>
2025-10-22 17:39:38 +0800

    refactor(translation): remove TranslationProviderService export from business_services.dart
    - Removed the export statement for TranslationProviderService in business_services.dart to reflect its deletion in the previous commit.

--- packages/server/lib/src/services/business/business_services.dart
+++ packages/server/lib/src/services/business/business_services.dart
@@ -5,7 +5,6 @@ export 'auth_service.dart';
 export 'config_service.dart';
 export 'permission_service.dart';
 export 'project_service.dart';
-export 'translation_provider_service.dart';
 export 'translation_service.dart';
 export 'user_service.dart';
 export 'user_settings_service.dart';
