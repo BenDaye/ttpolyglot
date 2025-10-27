94c93b434bd907f6e1a8c8283300c188d6d0757e
chuaer <1063195568@qq.com>
2025-10-27 17:10:23 +0800

    refactor(message_tips): improve authentication dialog handling
    - Added a flag to track the display state of the authentication dialog to prevent multiple instances from showing.
    - Enhanced the error handling logic to ensure the dialog is not displayed if the current route is the login page or if the dialog is already being shown.
    - Updated the dialog confirmation action to log out the user and redirect to the login page, improving user experience during authentication failures.

--- apps/ttpolyglot/lib/src/common/network/message_tips.dart
+++ apps/ttpolyglot/lib/src/common/network/message_tips.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:ttpolyglot/src/common/common.dart';
 import 'package:ttpolyglot/src/core/routing/app_pages.dart';
 import 'package:ttpolyglot_core/core.dart';
-import 'package:ttpolyglot_model/model.dart';
 
 final class MessageTips {
   MessageTips._();
+
+  // 标记是否正在显示鉴权弹窗
+  static bool _isShowingAuthDialog = false;
 
   static void showSuccessTips({
     required String message,
@@ -41,12 +44,24 @@ final class MessageTips {
     DataCodeEnum? status,
     ExtraModel? extra,
   }) {
-    // 鉴权失败处理
+    // 鉴权失败处理，并且路由不能等于登录页
     if (status == DataCodeEnum.unauthorized) {
+      // 如果当前路由等于登录页，则不处理
+      if ([Routes.signIn, Routes.signUp].contains(Get.currentRoute)) return;
+      // 如果已经显示了鉴权弹窗，则不重复显示
+      if (_isShowingAuthDialog) return;
+      // 标记正在显示鉴权弹窗
+      _isShowingAuthDialog = true;
+      // 其他路由
       DialogManager.showError(
         message,
         onConfirm: () async {
+          // 执行退出
+          await Get.find<AuthService>().logout();
+          // 跳转到登录页面
           await Get.offAllNamed(Routes.signIn);
+          // 重置标志
+          _isShowingAuthDialog = false;
           return true;
         },
       );
