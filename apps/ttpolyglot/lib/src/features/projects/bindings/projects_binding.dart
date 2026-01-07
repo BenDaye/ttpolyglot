907f31e2fd4bceb63dac9b4084de42580fe8dcc4
chuaer <1063195568@qq.com>
2026-01-07 16:13:51 +0800

    refactor(projects-binding): simplify dependencies method by removing unnecessary initialization

--- apps/ttpolyglot/lib/src/features/projects/bindings/projects_binding.dart
+++ apps/ttpolyglot/lib/src/features/projects/bindings/projects_binding.dart
@@ -1,9 +1,6 @@
 import 'package:get/get.dart';
-import 'package:ttpolyglot/src/features/features.dart';
 
 class ProjectsBinding extends Bindings {
   @override
-  void dependencies() {
-    Get.lazyPut(() => ProjectsController(), fenix: true);
-  }
+  void dependencies() {}
 }
