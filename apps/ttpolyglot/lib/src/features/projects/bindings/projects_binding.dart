4b58d22a528055b5ee8a4deb513ea350fc759d33
chuaer <1063195568@qq.com>
2026-01-07 16:15:00 +0800

    refactor(projects-binding): add lazy initialization for ProjectsController in dependencies method

--- apps/ttpolyglot/lib/src/features/projects/bindings/projects_binding.dart
+++ apps/ttpolyglot/lib/src/features/projects/bindings/projects_binding.dart
@@ -1,6 +1,9 @@
 import 'package:get/get.dart';
+import 'package:ttpolyglot/src/features/features.dart';
 
 class ProjectsBinding extends Bindings {
   @override
-  void dependencies() {}
+  void dependencies() {
+    Get.lazyPut(() => ProjectsController(), fenix: true);
+  }
 }
